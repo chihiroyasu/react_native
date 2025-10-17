6323afc1f3b2ba306e00a5adbe9e483963b214d9
Yasu <ychihiro0315@icloud.com>
2025-10-17 22:24:05 +0900

    カスタムコンポーネント(buttonとImageViewer)を作成

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,29 +1,52 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from 'expo-image-picker';
+import { StyleSheet, View } from "react-native";
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: "center",
+      // justifyContent: "center",
       alignItems: "center",
       backgroundColor: "#25292e",
     },
-    text: {
-      color: "#fff",
+    imageContainer: {
+      flex: 1,
     },
-    button: {
-      fontSize: 20,
-      textDecorationLine: 'underline',
-      color: '#fff',
-    }
-  })
+    footerContainer: {
+      flex: 1/3,
+      alignItems: "center",
+    },
+  });
+
+  const pickImageAsync = async () =>{
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if(!result.canceled){
+      console.log(result);
+    } else {
+      alert("You did not select any image.");
+    };
+  }
   return (
     <View
       style={styles.container}
     >
-      <Text style={styles.text}>Home Screen</Text>
-      <Link href="/about" style={styles.button}>Go to About Screen</Link>
+      <View style={styles.imageContainer}>
+        {/* <Image source={PlaceholderImage} style={styles.imageContainer} /> */}
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
